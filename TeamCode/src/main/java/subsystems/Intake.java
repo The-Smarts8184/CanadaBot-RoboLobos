234256dfbcff45bb0f203d2b0ba80fe168140d0f
sparky520 <144406148+sparky520@users.noreply.github.com>
2025-07-13 13:52:24 -0500

    boop

--- TeamCode/src/main/java/subsystems/Intake.java
+++ TeamCode/src/main/java/subsystems/Intake.java
@@ -1,9 +1,9 @@
 package subsystems;
 
 import com.arcrobotics.ftclib.command.Subsystem;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 import util.RobotConstants;
 import util.RobotHardware;
@@ -11,7 +11,7 @@ import util.RobotHardware;
 public class Intake implements Subsystem {
 
     private final RobotHardware robot;
-    private double turretTarget;
+    private double turretPosition;
     private ClawState clawState;
     private int target, prevTarget;
 
@@ -27,13 +27,78 @@ public class Intake implements Subsystem {
         CLOSED, OPEN
     }
 
-    public Intake(RobotHardware robot){
+    public Intake(RobotHardware robot) {
 
         this.robot = RobotHardware.getInstance();
 
         setExtensionTarget(RobotConstants.Intake.slideStowed);
         setTurretTarget(RobotConstants.Intake.turretStowed);
         slideSampleCheck = 0;
+
     }
 
+    public void setClawState(ClawState state) {
+        clawState = state;
+        switch (state) {
+            case OPEN:
+                robot.intakeClaw.setPosition(RobotConstants.Intake.clawOpen);
+                break;
+            case CLOSED:
+                robot.intakeClaw.setPosition(RobotConstants.Intake.clawClosed);
+                break;
+        }
+    }
+
+//    public void setState(IntakeState state) {
+//
+//    }
+
+    public void periodic() {
+        powerSlides();
+    }
+
+    public void setExtensionTarget(int position) {
+        position += slideSampleCheck;
+        if (position <= RobotConstants.Intake.slideMax && position >= RobotConstants.Intake.slideStowed) {
+            prevTarget = target;
+            target = position;
+        }
+    }
+
+    public int getExtensionTarget() {
+        return target;
+    }
+
+    public void powerSlides() {
+        double correction;
+        correction = robot.intakeSlidePID.calculate(robot.intakeSlide.getCurrentPosition(), target);
+
+        if (slideReset) {
+            robot.intakeSlide.setPower(-1); // MAYBE CHANGE
+            if (robot.intakeSlide.getCurrent(CurrentUnit.AMPS) > 5) {
+                robot.intakeSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                robot.intakeSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                robot.intakeSlide.setPower(0);
+
+                slideReset = false;
+            }
+        } else if (target == RobotConstants.Intake.slideStowed && (robot.intakeSlide.getCurrentPosition() - target) <= 20) {
+            robot.intakeSlide.setPower(0);
+        } else {
+            robot.intakeSlide.setPower(correction);
+        }
+    }
+
+    public void resetSlides() {
+        slideReset = true;
+    }
+
+    public void setTurretPosition(double position) {
+        turretPosition = position;
+        robot.turret.setPosition(position);
+    }
+
+    public double getTurretPosition() {
+         return robot.turret.getPosition();
+    }
 }
